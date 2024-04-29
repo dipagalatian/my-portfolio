523357b92366c813cfda0b808417859cd3ce8a9a
Dipa Galatian <dipagalatian3003@gmail.com>
2024-04-29 12:15:51 +0700

    delete: unused image and change cv file

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
                             className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold
                         uppercase bg-[#55e6a5] text-black flex items-center space-x-2"
                         >
-                            <Link href="/cv_parar_tria_dipa.pdf" target="_blank">
+                            <Link href="/ats_cv.pdf" target="_blank">
                                 Download CV
                             </Link>
                             <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
