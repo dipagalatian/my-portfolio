7e714276b4183d5e48719060959b123c525fd012
Dipa Galatian <dipagalatian3003@gmail.com>
2024-04-30 12:17:11 +0700

    public: change ats cv and creative cv

--- Components/About.tsx
+++ Components/About.tsx
@@ -26,8 +26,8 @@ const About = () => {
                         className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold
                         uppercase bg-[#55e6a5] text-black flex items-center space-x-2"
                     >
-                        <Link href="/ats_cv.pdf" target="_blank">
-                            Download CV
+                        <Link href="/creative_cv.pdf" target="_blank">
+                            Creative CV
                         </Link>
                         <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
                     </button>
